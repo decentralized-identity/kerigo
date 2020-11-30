5954a77ee73a5e344e8541cc95eec7a7c2c1f31d
Seth Back <sethjback@gmail.com>
2020-11-30 15:43:06 -0800

    move log tests into package

--- pkg/main_test.go
+++ pkg/main_test.go
@@ -3,12 +3,10 @@ package pkg
 import (
 	"crypto/ed25519"
 	"crypto/rand"
-	"encoding/json"
 	"testing"
 
 	"github.com/decentralized-identity/kerigo/pkg/derivation"
 	"github.com/decentralized-identity/kerigo/pkg/event"
-	"github.com/decentralized-identity/kerigo/pkg/log"
 	"github.com/decentralized-identity/kerigo/pkg/prefix"
 	"github.com/decentralized-identity/kerigo/pkg/version"
 	"github.com/stretchr/testify/assert"
@@ -68,45 +66,4 @@ func TestNew(t *testing.T) {
 
 	// This should be the same length as was calculated for the version string
 	assert.Equal(eventBytesExpected, len(eventBytes))
-}
-
-func TestLog(t *testing.T) {
-	assert := assert.New(t)
-
-	// events taken from keripy demo_sam
-	msg := []byte(`{"vs":"KERI10JSON0000fb_","pre":"ETT9n-TCGn8XfkGkcNeNmZgdZSwHPLyDsojFXotBXdSo","sn":"0","ilk":"icp","sith":"1","keys":["DSuhyBcPZEZLK-fcw5tzHn2N46wRCG_ZOoeKtWTOunRA"],"nxt":"EGAPkzNZMtX-QiVgbRbyAIZGoXvbGv9IPb0foWTZvI_4","toad":"0","wits":[],"cnfg":[]}`)
-	e := &event.Event{}
-	err := json.Unmarshal(msg, e)
-	assert.Nil(err)
-
-	sig := []byte(`-AABAAtf0OqrkGmK3vdMcS5E3mLxeFh14SbvCNjZnZrxAazgYTemZc1S-Pr0ge9IQuHesmh8cJncRkef1PgxFavDKqDQ`)
-	d, extra, err := derivation.ParseAttachedSignatures(sig)
-	assert.Nil(err)
-	assert.Empty(extra)
-	assert.Len(d, 1)
-
-	keyD, err := e.KeyDerivation(0)
-	assert.Nil(err)
-
-	assert.True(ed25519.Verify(keyD.Raw, msg, d[0].Raw))
-
-	l := &log.Log{}
-	err = l.Apply(e)
-	assert.Nil(err)
-
-	nxt := []byte(`{"vs":"KERI10JSON0000a3_","pre":"ETT9n-TCGn8XfkGkcNeNmZgdZSwHPLyDsojFXotBXdSo","sn":"1","ilk":"ixn","dig":"EixO2SBNow3tYDfYX6NRt1O9ZSMx2IsBeWkh8YJRp5VI","data":[]}`)
-	e = &event.Event{}
-	err = json.Unmarshal(nxt, e)
-	assert.Nil(err)
-
-	sig = []byte(`-AABAAy0fxup1pAatbj9IneWbFLp2qcozBVOFnmjlbf4Sr8QNL2byHOth3M3r-_3Eu5C9xmywPHZtixt-wc5eFqXxhAw`)
-	d, extra, err = derivation.ParseAttachedSignatures(sig)
-	assert.Nil(err)
-	assert.Empty(extra)
-	assert.Len(d, 1)
-
-	assert.True(ed25519.Verify(keyD.Raw, nxt, d[0].Raw))
-
-	err = l.Apply(e)
-	assert.Nil(err)
 }
